363f6f90557c85bb132c3d79be5c116e82f7ac76
XiaoPan <2489964425@qq.com>
2021-01-02 15:12:22 +0800

    查找子节点

--- dist/jquery.7a6e0748.js
+++ dist/jquery.7a6e0748.js
@@ -118,6 +118,18 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"jquery.js":[function(require,module,exports) {
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 window.jQuery = function (selectorOrArray) {
   var elements;
 
@@ -164,6 +176,13 @@ window.jQuery = function (selectorOrArray) {
         if (array.indexOf(node.parentNode) === -1) {
           array.push(node.parentNode);
         }
+      });
+      return jQuery(array);
+    },
+    children: function children() {
+      var array = [];
+      this.each(function (node) {
+        array.push.apply(array, _toConsumableArray(node.children));
       });
       return jQuery(array);
     },
